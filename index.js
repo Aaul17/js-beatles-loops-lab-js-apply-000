04458bdf17c601c72eedbea5864d53259f8bc258
Alexandra Aulum <alex.aulum@gmail.com>
2018-07-23 14:14:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  for (var i = 0; i < musicians.length; i++) {
+    console.log('${musicians[i]} plays ${instruments[i]}');
+  }
+  return theBeatlesPlay();
+}
